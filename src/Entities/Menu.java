2f52079dec6d6be3b1f6a626dfbe2fcdb9859d8e
bkantor1 <bkantor01@gmail.com>
2013-03-23 14:44:20 -0400

    Renamed Menu Command; Implemented Menu;

--- src/Entities/Menu.java
+++ src/Entities/Menu.java
@@ -1,8 +1,30 @@
 
 package Entities;
 
+import java.util.ArrayList;
+
 
 public class Menu {
+	private Menu_Option cutCommand,pasteCommand,insertCommand,indentCommand;
+	public Menu(Menu_Option cut, Menu_Option paste, Menu_Option insert, Menu_Option indent){
+		cutCommand = cut;
+		pasteCommand = paste;
+		insertCommand = insert;
+		indentCommand = indent;
+	}
 
+	public void cut(){
+		cutCommand.execute();
+	}
+	public void paste(){
+		pasteCommand.execute();
+	}
+	public void insert(){
+		insertCommand.execute();		
+	}
+	public void indent(){
+		indentCommand.execute();
+	}
+	
 }
 
