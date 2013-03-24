9992e6f23c3a737a71dd267370171797d724b58f
bkantor1 <bkantor01@gmail.com>
2013-03-24 16:25:16 -0400

    Edited implementation of InsertCommand

--- src/Commands/InsertCommand.java
+++ src/Commands/InsertCommand.java
@@ -2,19 +2,21 @@ package Commands;
 
 import javax.swing.text.html.HTML;
 
+import Entities.Document;
 import Entities.Menu_Option;
 
 public class InsertCommand implements Menu_Option{
-
+	private Document doc;
+	public InsertCommand(Document document){
+		doc = document;
+	}
 	@Override
 	public void execute(HTML.Tag tag) {
-		// TODO Auto-generated method stub
 		
 	}
 
     @Override
     public void execute() {
-        // TODO Auto-generated method stub
         
     }
 
