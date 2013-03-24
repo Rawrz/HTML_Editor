77d5c26429cc8bcc023c61c1b81d2f3808fb1b12
Ben Kantor <bdk3079@rit.edu>
2013-03-24 12:00:24 -0400

    Implemented CloseCommmand and OpenCommand

--- src/Commands/CloseCommand.java
+++ src/Commands/CloseCommand.java
@@ -1,13 +1,19 @@
 package Commands;
 
+import Entities.HTML_Editor;
 import Entities.Menu_Option;
 
 public class CloseCommand implements Menu_Option{
-
+	private HTML_Editor htmlEditor;
+	
+	public CloseCommand(HTML_Editor editor){
+		htmlEditor = editor;
+	}
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		//TODO: Put real document int in here
+		int i = 0;
+		htmlEditor.closeDocument(i);
 	}
 
 }
--- src/Commands/OpenCommand.java
+++ src/Commands/OpenCommand.java
@@ -1,13 +1,18 @@
 package Commands;
 
+import Entities.HTML_Editor;
 import Entities.Menu_Option;
 
 public class OpenCommand implements Menu_Option{
-
+	
+	private HTML_Editor htmlEditor;
+	public OpenCommand(HTML_Editor editor){
+		htmlEditor = editor;
+	}
+	
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		htmlEditor.openDocument();
 	}
 
 }
