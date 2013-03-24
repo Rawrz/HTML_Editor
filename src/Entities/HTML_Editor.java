1d87dc366f55f28ad7081b347f604c4d470da1ca
Chris Timmons <cwt2617@rit.edu>
2013-03-23 23:11:19 -0400

    Added some methods to the HTML_Editor and Proxy_document to do basic stuff. Need more info on structure to add more.

--- src/Entities/HTML_Editor.java
+++ src/Entities/HTML_Editor.java
@@ -1,15 +1,31 @@
-
 package Entities;
 
+import java.util.*;
 
 public class HTML_Editor {
-
+	
+	private ArrayList<Document> docs;
+	
     /**
      * @param args
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
+    }
+    
+    public void newDocument() {
+    	Proxy_Document proxy = new Proxy_Document();
+    	docs.add(proxy);
+    }
+    
+    public void openDocument() {
+    	
+    }
+    
+    public void closeDocument(int i) {
+    	docs.get(i).close();
+    	docs.remove(i);
     }
 
 }
--- src/Entities/Proxy_Document.java
+++ src/Entities/Proxy_Document.java
@@ -1,28 +1,32 @@
 package Entities;
 
 public class Proxy_Document implements Document {
+	
+	
+	public Proxy_Document() {
+	}
 
 	@Override
 	public void cut() {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 	@Override
 	public void paste() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void save() {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	@Override
 	public void saveAs() {
-		// TODO Auto-generated method stub
+		save();
 
 	}
 
