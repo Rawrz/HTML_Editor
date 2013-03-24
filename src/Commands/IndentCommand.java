18cea6251f2084b0c0fef5ec7f9f0a0a746ecda9
Ben Kantor <bkantor01@gmail.com>
2013-03-24 11:10:39 -0400

    Created Command package; added commands to package

---
+++ src/Commands/IndentCommand.java
@@ -0,0 +1,13 @@
+package Commands;
+
+import Entities.Menu_Option;
+
+public class IndentCommand implements Menu_Option{
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Commands/PasteCommand.java
@@ -0,0 +1,13 @@
+package Commands;
+
+import Entities.Menu_Option;
+
+public class PasteCommand implements Menu_Option{
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
