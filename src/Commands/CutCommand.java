e6f43b960a22144c3d51b00bb400baa339039510
bkantor1 <bkantor01@gmail.com>
2013-03-24 12:58:08 -0400

    Implemented CutCommand for executing cut for a documents

--- src/Commands/CutCommand.java
+++ src/Commands/CutCommand.java
@@ -1,13 +1,18 @@
 package Commands;
 
+import Entities.Document;
 import Entities.Menu_Option;
 
 public class CutCommand implements Menu_Option{
-
+	
+	private Document doc;
+	
+	public CutCommand(Document document){
+		doc = document;
+	}
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		doc.cut();
 	}
 
 }
