7c3359d8bf88771634e91ee468d892c1b1ad7402
bkantor1 <bkantor01@gmail.com>
2013-03-24 13:00:34 -0400

    Implemented PasteCommand for executing pasting to documents

--- src/Commands/PasteCommand.java
+++ src/Commands/PasteCommand.java
@@ -1,13 +1,18 @@
 package Commands;
 
+import Entities.Document;
 import Entities.Menu_Option;
 
 public class PasteCommand implements Menu_Option{
-
+	
+	private Document doc;
+	
+	public PasteCommand(Document document){
+		doc = document;
+	}
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		doc.paste();		
 	}
 
 }
