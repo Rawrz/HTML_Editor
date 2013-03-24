da333ecab1f7c4cb569151e6abf850252046b548
bkantor1 <bkantor01@gmail.com>
2013-03-24 12:50:10 -0400

    Implemented save document command SaveCommand

--- src/Commands/SaveCommand.java
+++ src/Commands/SaveCommand.java
@@ -1,13 +1,16 @@
 package Commands;
 
+import Entities.Document;
 import Entities.Menu_Option;
 
 public class SaveCommand implements Menu_Option{
-
+	private Document doc;
+	public SaveCommand(Document document){
+		doc = document;
+	}
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		doc.save();
 	}
 
 }
