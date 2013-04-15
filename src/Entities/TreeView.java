b29b146ea78869885e7ee1d720178cb54ab3affd
Rawrz <rokpara@gmail.com>
2013-04-14 22:45:02 -0400

    Changed name of HTMLTreeGUI to treeview

--- src/Entities/TreeView.java
+++ src/Entities/TreeView.java
@@ -5,11 +5,13 @@ import javax.swing.JFrame;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 
-public class HTMLTreeGUI extends JFrame {
+public class TreeView extends JFrame {
     JTree tree;
-    public HTMLTreeGUI(DefaultMutableTreeNode node){
+    
+    public TreeView(DefaultMutableTreeNode node){
         tree = new JTree(node);
         this.add(tree);
+        this.setSize(500, 500);
         this.setVisible(true);
         this.pack();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
