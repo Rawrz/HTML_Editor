b0c1ddcc49922eab449e770999abdded0ffed004
Ben Kantor <bdk3079@rit.edu>
2013-04-19 02:59:11 -0400

    Fixed un-handled warnings in TreeView class

--- src/GUI/TreeView.java
+++ src/GUI/TreeView.java
@@ -2,20 +2,19 @@
 package GUI;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
-import javax.swing.tree.TreeCellRenderer;
 
+/**
+ * JPanel for Displaying Outline Mode 
+ */
+@SuppressWarnings("serial")
 public class TreeView extends JPanel {
     private JTree tree;
-    private DocumentGUI doc;
-    private String title;
     private JScrollPane scrollPane;
     
     public TreeView(DocumentGUI docParam, String titleParam, DefaultMutableTreeNode node){
@@ -33,8 +32,7 @@ public class TreeView extends JPanel {
     	tree.setCellRenderer(cellRenderer);
     	tree.setFont(new Font(Font.DIALOG,Font.PLAIN,20));
         scrollPane = new JScrollPane(tree);
-        add(scrollPane);
-        
+        add(scrollPane);        
         setPreferredSize(new Dimension(300, 500));
     }
 
